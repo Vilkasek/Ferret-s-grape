764afd3270fad864beb24588dcd3b0c6c5478b7c
Jakub Brandt <tv.brandon.nr1@gmail.com>
2025-05-18 09:29:39 +0200

    Zaimplementowałem do main.cpp stany

--- main.cpp
+++ main.cpp
@@ -7,15 +7,40 @@
 
 #include <raylib.h>
 
+#include "utils/states/states.h"
+
 PSP_MODULE_INFO("Ferret\'s Grape", 0, 0, 1);
 PSP_MAIN_THREAD_ATTR(THREAD_ATTR_USER | THREAD_ATTR_VFPU);
 
-void update() {}
+States &states = States::getInstance();
+
+void update() {
+  switch (states.get_state()) {
+  case States::State::MAIN_MENU:
+    break;
+  case States::State::GAME:
+    break;
+  case States::State::GAME_OVER:
+    break;
+  }
+}
 void render() {
   BeginDrawing();
 
-  ClearBackground(RAYWHITE);
-  DrawText("Hello, World!", 140, 120, 20, BLACK);
+  switch (states.get_state()) {
+  case States::State::MAIN_MENU:
+    ClearBackground(RAYWHITE);
+    DrawText("Hello, World!", 140, 120, 20, BLACK);
+    break;
+  case States::State::GAME:
+    ClearBackground(LIGHTGRAY);
+    DrawText("Hello, World!", 140, 120, 20, BLACK);
+    break;
+  case States::State::GAME_OVER:
+    ClearBackground(DARKGREEN);
+    DrawText("Hello, World!", 140, 120, 20, BLACK);
+    break;
+  }
 
   EndDrawing();
 }

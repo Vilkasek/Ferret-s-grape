8ea608351bd9c5b752b04c3ef74fc875c0379567
Jakub Brandt <tv.brandon.nr1@gmail.com>
2025-05-18 09:23:38 +0200

    Dodałem do klasy stanów setter i getter

--- utils/states.cpp
+++ utils/states.cpp
@@ -11,9 +11,14 @@ public:
     GAME_OVER,
   };
 
+  State get_state() { return currentState; }
+
+  void set_state(State newState) { currentState = newState; }
+
   States(const States &) = delete;
   States &operator=(const States &) = delete;
 
 private:
   States() {}
+  State currentState = State::GAME;
 };
